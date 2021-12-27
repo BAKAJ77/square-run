e11806bdf85f95dc78c7ddd5be9240795524fe84
Benjamin Aziz Khalil Ajayi <kabbaben@hotmail.co.uk>
2021-12-27 00:19:12 +0000

    Changes accomodating for the relocating of the premake executable

--- generate-project.bat
+++ generate-project.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p type="Enter the type of project files to generate: "
 
-cd %~dp0\bin\premake 
-premake5 --file=..\..\premake.lua %type%
+cd %~dp0\bin\premake\win64 
+premake5 --file=..\..\..\premake.lua %type%
 pause
--- premake.lua
+++ premake.lua
@@ -6,20 +6,20 @@ workspace "SquareRun"
 
 project "SquareRun"
     location "%{prj.name}"
-
     staticruntime "on"
     language "C++"
     cppdialect "C++17"
-
     targetdir "bin/game/"
-    objdir "objs/%{cfg.buildcfg}-%{cfg.architecture}/"
 
-    includedirs { "%{prj.name}/src" }
-    files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp", "%{prj.name}/src/**.tpp" }
+    includedirs { "%{prj.name}/src", "libs/boost/boost", "libs/fmod/core/inc", "libs/freetype/include",
+        "libs/glad/include", "libs/glfw/include", "libs/glm", "libs/stb" }
+
+    files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp", "%{prj.name}/src/**.c", "%{prj.name}/src/**.tpp" }
 
     -- Project platform define macro based on identified system
     filter "system:windows"
         defines "_PLATFORM_WINDOWS"
+        libdirs { "bin/fmod/win64", "bin/freetype/win64", "bin/glfw/win64" }
 
     filter "system:macosx"
         defines "_PLATFORM_MACOSX"
@@ -27,15 +27,28 @@ project "SquareRun"
     -- Project settings with values unique to the Debug/Release configurations
     filter "configurations:Debug"
         kind "ConsoleApp"
-        targetname "SquareRun-Dbg"
+        objdir "objs/debug"
+        targetname "SquareRun-dbg"
+
+        links { "glfw3-dbg", "freetype-dbg", "fmodL_vc" }
         defines { "_DEBUG" }
         symbols "On"
 
     filter "configurations:Release"
         kind "WindowedApp"
+        objdir "objs/release"
         targetname "SquareRun"
-        entrypoint "mainCRTStartup"
+
+        links { "glfw3", "freetype", "fmod_vc" }
         defines { "NDEBUG" }
         optimize "Speed"
+        entrypoint "mainCRTStartup"
+
+    -- Copy required DLL lib files into game executable directory when building
+    filter { "system:windows", "configurations:Debug" }
+        postbuildcommands { "copy ..\\bin\\fmod\\win64\\fmodL.dll ..\\bin\\game\\fmodL.dll" }
+
+    filter { "system:windows", "configurations:Release" }
+        postbuildcommands { "copy ..\\bin\\fmod\\win64\\fmod.dll ..\\bin\\game\\fmod.dll" }
 
 ------------------------------------------------------------------------------------------------------------------------------------------------
