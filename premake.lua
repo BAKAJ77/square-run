751d31ed15bc901abf1cb1d16d24338d7cedb4dd
BAKAJ77 <44305796+BAKAJ77@users.noreply.github.com>
2022-03-26 01:52:22 +0000

    Update premake script

--- premake.lua
+++ premake.lua
@@ -1,52 +1,56 @@
-workspace "SquareRun"
-    configurations { "Debug", "Release" }
+workspace "square-run"
+    configurations { "debug", "release" }
     architecture "x86_64"
 
 ------------------------------------------------------------------------------------------------------------------------------------------------
 
-project "SquareRun"
+project "square-run"
     location "%{prj.name}"
     staticruntime "on"
     language "C++"
     cppdialect "C++17"
-    targetdir "bin/game/"
 
-    includedirs { "%{prj.name}/src", "libs/asio/include", "libs/irrklang/include", "libs/freetype/include", "libs/glad/include",
-        "libs/glfw/include", "libs/glm", "libs/stb", "libs/json/single_include" }
+    targetname "square-run"
+    targetdir "bin/%{cfg.buildcfg}/"
+    objdir "objs/%{prj.name}/%{cfg.buildcfg}/"
+
+    includedirs { "%{prj.name}/src", "libs/asio/asio/include", "libs/irrklang/include", "libs/freetype/include", "libs/glad/include",
+        "libs/glfw/include", "libs/glm", "libs/stb", "libs/json/include" }
 
     files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp", "%{prj.name}/src/**.c", "%{prj.name}/src/**.tpp" }
+
+    links { "glfw3", "freetype", "irrKlang" }
 
     -- Project platform define macro based on identified system
     filter "system:windows"
         defines "_PLATFORM_WINDOWS"
-        libdirs { "bin/irrklang/win64", "bin/freetype/win64", "bin/glfw/win64" }
 
     filter "system:macosx"
         defines "_PLATFORM_MACOSX"
 
     -- Project settings with values unique to the Debug/Release configurations
-    filter "configurations:Debug"
+    filter "configurations:debug"
         kind "ConsoleApp"
-        objdir "objs/debug"
-        targetname "SquareRun-dbg"
-
-        links { "glfw3-dbg", "freetype-dbg", "irrKlang", }
+        
+        libdirs { "libs/irrklang/bin", "libs/freetype/bin/debug", "libs/glfw/bin/debug" }
         defines { "_DEBUG" }
         symbols "On"
 
-    filter "configurations:Release"
+    filter "configurations:release"
         kind "WindowedApp"
-        objdir "objs/release"
-        targetname "SquareRun"
-
-        links { "glfw3", "freetype", "irrKlang" }
-        defines { "NDEBUG" }
-        optimize "Speed"
         entrypoint "mainCRTStartup"
 
+        libdirs { "libs/irrklang/bin", "libs/freetype/bin/release", "libs/glfw/bin/release" }
+        defines { "NDEBUG" }
+        optimize "Speed"
+
     -- Copy required DLL lib files into game executable directory when building
-    filter { "system:windows" }
-        postbuildcommands { "copy ..\\bin\\irrklang\\win64\\irrKlang.dll ..\\bin\\game\\irrKlang.dll",
-            "copy ..\\bin\\irrklang\\win64\\ikpMP3.dll ..\\bin\\game\\ikpMP3.dll" }
+    filter { "system:windows", "configurations:debug" }
+        postbuildcommands { "copy ..\\libs\\irrklang\\bin\\irrKlang.dll ..\\bin\\debug\\irrKlang.dll",
+            "copy ..\\libs\\irrklang\\bin\\ikpMP3.dll ..\\bin\\debug\\ikpMP3.dll" }
+
+    filter { "system:windows", "configurations:release" }
+        postbuildcommands { "copy ..\\libs\\irrklang\\bin\\irrKlang.dll ..\\bin\\release\\irrKlang.dll",
+            "copy ..\\libs\\irrklang\\bin\\ikpMP3.dll ..\\bin\\release\\ikpMP3.dll" }
 
 ------------------------------------------------------------------------------------------------------------------------------------------------
